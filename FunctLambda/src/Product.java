0242201aaf3779c2451619bfc7cf270a6bac0076
SaduneniNikhil <naninikhil20@gmail.com>
2019-10-30 14:13:18 +0530

    2nd Commit with Branch

---
+++ FunctLambda/src/Product.java
@@ -0,0 +1,20 @@
+
+public class Product {
+
+private String pname;
+private int price;
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public int getPrice() {
+	return price;
+}
+public void setPrice(int price) {
+	this.price = price;
+}
+
+	
+}
